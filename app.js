abdad933af0858fb3c2fa7384bde96449c5e38c0
Rulo Kobashikawa <akobashikawa@gmail.com>
2015-12-29 10:11:37 -0500

    Resolve error template. Delete layout page.

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.engine('html', function(filePath, options, callback) {
         if (err) return callback(new Error(err));
         var html = content.toString();
         $ = cheerio.load(html);
-        options.replaces($);
+        if (typeof options.replaces === 'function') {
+            options.replaces($);
+        }
         var rendered = $.html();
         return callback(null, rendered);
     });
@@ -52,10 +54,15 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
+        res.status(err.status || 500);console.log(err.status);
         res.render('error', {
-            message: err.message,
-            error: err
+            replaces: function($) {
+                $('title').text('Error');
+                $('h1.title').text('Error');
+                $('.content .message').text(err.message);
+                $('.content .status').text(err.status.toString());
+                $('.content .stack').text(err.stack);
+            }
         });
     });
 }
@@ -65,8 +72,13 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message,
-        error: {}
+        replaces: function($) {
+            $('title').text('Error');
+            $('h1.title').text('Error');
+            $('.content .message').text(err.message);
+            $('.content .status').text(err.status.toString());
+            $('.content .stack').text('');
+        }
     });
 });
 
